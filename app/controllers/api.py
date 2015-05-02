f021a9ff49c41d67b06dceb60713ed271c9b1260
Yax <1949284+kianby@users.noreply.github.com>
2015-05-02 20:24:56 +0200

    Progress on API to retrieve comments

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -2,11 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from flask import request, jsonify
+from playhouse.shortcuts import model_to_dict
 from app import app
+from app.models.site import Site
+from app.models.comment import Comment
 
 logger = logging.getLogger(__name__)
 
 
-@app.route("/comments", methods=['GET'])
-def get_comments():
-    return "OK"
+@app.route("/comments", methods=['POST'])
+def query_comments():
+
+    query = request.json
+    token = query['token']
+    url = query['url']
+    logger.info('token=%s url=%s' % (token, url))
+
+    comments = []
+    for comment in Comment.select(Comment).join(Site).where(
+           (Comment.url == url) &
+           (Site.token == token)).order_by(Comment.published):
+        comments.append(model_to_dict(comment))
+
+    r = jsonify({'data': comments})
+    r.status_code = 200
+    return r
