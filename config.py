91344ab775f6efa46b27d6a3596320d2dd3d7cfe
Yax <kianby@madyanne.fr>
2015-05-16 18:12:18 +0200

    Send comment by email via SRMail

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 
 DEBUG = True
 
-LANG = "en" 
+LANG = "fr"
 
 #DB_URL = "mysql://stacosys_user:stacosys_password@localhost:3306/stacosys"
 DB_URL = "sqlite:///db.sqlite"
 
-MAIL_URL = "http://localhost:8025"
+MAIL_URL = "http://localhost:8025/mbox"
 
 HTTP_ADDRESS = "0.0.0.0"
 HTTP_PORT = 8000
