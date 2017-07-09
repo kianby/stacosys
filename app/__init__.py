c5ae33f73888ad9552e5e4ab51222b56ee77f640
Yax <kianby@madyanne.fr>
2017-07-09 17:13:41 +0200

    one cache per worker

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,3 @@
-import time
 from sanic import Sanic
 
 app = Sanic()
-cache = {}
-cache_time = 0
-
-def get_cached(key):
-    global cache
-    global cache_time
-    value = cache.get(key,None)
-    if (time.time() - cache_time) > 120:
-        cache = {}
-        cache_time = time.time()
-    return value
-
-def set_cached(key, value):
-    global cache
-    cache[key] = value
