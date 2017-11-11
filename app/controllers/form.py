eecea8c66105d96240ca134459e275c920a9ddeb
Yax <1949284+kianby@users.noreply.github.com>
2017-11-11 07:33:21 +0100

    Add HTML form entry point

---
+++ app/controllers/form.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import config
+from flask import request, jsonify, abort
+from app import app
+from app.models.site import Site
+from app.models.comment import Comment
+from app.helpers.hashing import md5
+from app.services import processor
+
+logger = logging.getLogger(__name__)
+
+@app.route("/newcomment", methods=['POST'])
+def new_form_comment():
+
+    try:
+        data = request.form
+        logger.info(data)
+
+        # validate token: retrieve site entity
+        token = data.get('token', '')
+        site = Site.select().where(Site.token == token).get()
+        if site is None:
+            logger.warn('Unknown site %s' % token)
+            abort(400)
+
+        # honeypot for spammers
+        captcha = data.get('captcha', '')
+        if captcha:
+            logger.warn('discard spam: data %s' % data)
+            abort(400)
+
+        processor.enqueue({'request': 'new_comment', 'data': data})
+
+    except:
+        logger.exception("new comment failure")
+        abort(400)
+
+    return "OK"
--- app/run.py
+++ app/run.py
@@ -20,6 +20,8 @@ import config
 from app.services import database
 from app.services import processor
 from app.controllers import api
+from app.controllers import form
+from app.controllers import report
 from app.controllers import mail
 from app.controllers import reader
 from app import app
