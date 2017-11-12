cff35798a763bc353458bf0f0cc0d4effc1a4a03
Yax <1949284+kianby@users.noreply.github.com>
2017-11-12 18:20:05 +0100

    upd config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_URL = "sqlite:///db.sqlite"
 
 MAIL_URL = "http://localhost:8025/mbox"
 
-HTTP_ADDRESS = "0.0.0.0"
+HTTP_ADDRESS = "127.0.0.1"
 HTTP_PORT = 8100
 HTTP_WORKERS = 1
 CORS_ORIGIN = "*"
@@ -18,7 +18,7 @@ SALT = "BRRJRqXgGpXWrgTidBPcixIThHpDuKc0"
 
 SECRET = "Uqca5Kc8xuU6THz9"
 
-ROOT_URL = 'http://localhost:8000'
+ROOT_URL = 'http://localhost:8100'
 
 RSS_URL_PROTO = 'http'
 RSS_FILE = 'comments.xml'
