44ea468b712828281f57e01fce256f26517fddfc
Yax <1949284+kianby@users.noreply.github.com>
2020-03-31 19:53:59 +0200

    add type checking

--- stacosys/model/email.py
+++ stacosys/model/email.py
@@ -2,7 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 from typing import NamedTuple
+from typing import List
 from datetime import datetime
+
+
+class Part(NamedTuple):
+    content: str
+    content_type: str
+
+
+class Attachment(NamedTuple):
+    filename: str
+    content: str
+    content_type: str
+
 
 class Email(NamedTuple):
     id: int
@@ -11,4 +24,6 @@ class Email(NamedTuple):
     from_addr: str
     to_addr: str
     subject: str
-    content: str
+    parts: List[Part]
+    attachments: List[Attachment]
+    plain_text_content: str = 'no plain-text part'
