b258194d6f25c0bfc067aa04d736f39b8af1d38c
Yax <1949284+kianby@users.noreply.github.com>
2020-12-27 20:04:20 +0100

    tinydb migration

--- stacosys/core/database.py
+++ stacosys/core/database.py
@@ -15,3 +15,24 @@ def setup():
     from stacosys.model.comment import Comment
 
     get_db().create_tables([Site, Comment], safe=True)
+
+from playhouse.shortcuts import model_to_dict
+import json
+
+def tojson_model(comment):
+    dcomment = model_to_dict(comment)
+    del dcomment['site']
+    tcomment = json.dumps(dcomment, indent=4, sort_keys=True, default=str)
+    return json.loads(tcomment) 
+
+def tojson_models(models):
+    print(json.dumps(list(models.dicts()), indent=4, sort_keys=True, default=str))
+
+def dump_db():
+    from tinydb import TinyDB, Query
+    from stacosys.model.comment import Comment
+    db = TinyDB('db.json')
+    for comment in Comment.select():
+        cc = tojson_model(comment)
+        print(cc)
+        db.insert(cc)
