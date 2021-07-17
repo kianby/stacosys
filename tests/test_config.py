b9039a4eb7d1377f1f4fd13c910efe72e559a67b
Yax <1949284+kianby@users.noreply.github.com>
2021-07-17 18:32:29 +0200

    Improve unit test

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,44 +13,36 @@ EXPECTED_IMAP_LOGIN = "user"
 
 class ConfigTestCase(unittest.TestCase):
 
-    def conf(self):
-        conf = Config()
-        conf.put(ConfigParameter.DB_SQLITE_FILE, EXPECTED_DB_SQLITE_FILE)
-        conf.put(ConfigParameter.HTTP_PORT, EXPECTED_HTTP_PORT)
-        conf.put(ConfigParameter.IMAP_PORT, EXPECTED_IMAP_PORT)
-        conf.put(ConfigParameter.SMTP_STARTTLS, "yes")
-        conf.put(ConfigParameter.IMAP_SSL, "false")
-        return conf
+    def setUp(self):
+        self.conf = Config()
+        self.conf.put(ConfigParameter.DB_SQLITE_FILE, EXPECTED_DB_SQLITE_FILE)
+        self.conf.put(ConfigParameter.HTTP_PORT, EXPECTED_HTTP_PORT)
+        self.conf.put(ConfigParameter.IMAP_PORT, EXPECTED_IMAP_PORT)
+        self.conf.put(ConfigParameter.SMTP_STARTTLS, "yes")
+        self.conf.put(ConfigParameter.IMAP_SSL, "false")
 
     def test_exists(self):
-        conf = self.conf()
-        self.assertTrue(conf.exists(ConfigParameter.DB_SQLITE_FILE))
-        self.assertFalse(conf.exists(ConfigParameter.IMAP_HOST))
+        self.assertTrue(self.conf.exists(ConfigParameter.DB_SQLITE_FILE))
+        self.assertFalse(self.conf.exists(ConfigParameter.IMAP_HOST))
 
     def test_get(self):
-        conf = self.conf()
-        self.assertEqual(conf.get(ConfigParameter.DB_SQLITE_FILE), EXPECTED_DB_SQLITE_FILE)
-        self.assertEqual(conf.get(ConfigParameter.HTTP_PORT), EXPECTED_HTTP_PORT)
-        self.assertIsNone(conf.get(ConfigParameter.HTTP_HOST))
-        self.assertEqual(conf.get(ConfigParameter.HTTP_PORT), EXPECTED_HTTP_PORT)
-        self.assertEqual(conf.get(ConfigParameter.IMAP_PORT), EXPECTED_IMAP_PORT)
-        self.assertEqual(conf.get_int(ConfigParameter.IMAP_PORT), int(EXPECTED_IMAP_PORT))
+        self.assertEqual(self.conf.get(ConfigParameter.DB_SQLITE_FILE), EXPECTED_DB_SQLITE_FILE)
+        self.assertEqual(self.conf.get(ConfigParameter.HTTP_PORT), EXPECTED_HTTP_PORT)
+        self.assertIsNone(self.conf.get(ConfigParameter.HTTP_HOST))
+        self.assertEqual(self.conf.get(ConfigParameter.HTTP_PORT), EXPECTED_HTTP_PORT)
+        self.assertEqual(self.conf.get(ConfigParameter.IMAP_PORT), EXPECTED_IMAP_PORT)
+        self.assertEqual(self.conf.get_int(ConfigParameter.IMAP_PORT), int(EXPECTED_IMAP_PORT))
+        self.assertEqual(self.conf.get_int(ConfigParameter.HTTP_PORT), 8080)
+        self.assertTrue(self.conf.get_bool(ConfigParameter.SMTP_STARTTLS))
+        self.assertFalse(self.conf.get_bool(ConfigParameter.IMAP_SSL))
         try:
-            conf.get_int(ConfigParameter.HTTP_PORT)
+            self.conf.get_bool(ConfigParameter.DB_SQLITE_FILE)
             self.assertTrue(False)
-        except Exception:
-            pass
-        self.assertTrue(conf.get_bool(ConfigParameter.SMTP_STARTTLS))
-        self.assertFalse(conf.get_bool(ConfigParameter.IMAP_SSL))
-        try:
-            conf.get_bool(ConfigParameter.DB_URL)
-            self.assertTrue(False)
-        except Exception:
+        except AssertionError:
             pass
 
     def test_put(self):
-        conf = self.conf()
-        self.assertFalse(conf.exists(ConfigParameter.IMAP_LOGIN))
-        conf.put(ConfigParameter.IMAP_LOGIN, EXPECTED_IMAP_LOGIN)
-        self.assertTrue(conf.exists(ConfigParameter.IMAP_LOGIN))
-        self.assertEqual(conf.get(ConfigParameter.IMAP_LOGIN), EXPECTED_IMAP_LOGIN)
+        self.assertFalse(self.conf.exists(ConfigParameter.IMAP_LOGIN))
+        self.conf.put(ConfigParameter.IMAP_LOGIN, EXPECTED_IMAP_LOGIN)
+        self.assertTrue(self.conf.exists(ConfigParameter.IMAP_LOGIN))
+        self.assertEqual(self.conf.get(ConfigParameter.IMAP_LOGIN), EXPECTED_IMAP_LOGIN)
