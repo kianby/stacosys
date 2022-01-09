895efb5257981ffc8dafdebd5a66651360af5756
Yax <1949284+kianby@users.noreply.github.com>
2022-01-09 11:04:28 +0100

    add unit test

--- tests/test_imap.py
+++ tests/test_imap.py
@@ -4,6 +4,7 @@
 import datetime
 import unittest
 from email.header import Header
+from email.message import Message
 
 from stacosys.core import imap
 
@@ -24,3 +25,9 @@ class ImapTestCase(unittest.TestCase):
         self.assertEqual(parsed.month, 12)
         self.assertEqual(parsed.year, 2021)
         # do not compare hours. don't care about timezone
+
+    def test_to_plain_text_content(self):
+        msg = Message()
+        payload = b"non\r\n\r\nLe 08/12/2021 \xc3\xa0 20:04, kianby@free.fr a \xc3\xa9crit\xc2\xa0:\r\n> Bonjour,\r\n>\r\n> Un nouveau commentaire a \xc3\xa9t\xc3\xa9 post\xc3\xa9 pour l'article /2021/rester-discret-sur-github//\r\n>\r\n> Vous avez deux r\xc3\xa9ponses possibles :\r\n> - rejeter le commentaire en r\xc3\xa9pondant NO (ou no),\r\n> - accepter le commentaire en renvoyant cet email tel quel.\r\n>\r\n> Si cette derni\xc3\xa8re option est choisie, Stacosys publiera le commentaire tr\xc3\xa8s bient\xc3\xb4t.\r\n>\r\n> Voici les d\xc3\xa9tails concernant le commentaire :\r\n>\r\n> author: ET Rate\r\n> site:\r\n> date: 2021-12-08 20:03:58\r\n> url: /2021/rester-discret-sur-github//\r\n>\r\n> gfdgdgf\r\n>\r\n>\r\n> --\r\n> Stacosys\r\n"
+        msg.set_payload(payload, "UTF-8")
+        self.assertTrue(imap._to_plain_text_content(msg))
