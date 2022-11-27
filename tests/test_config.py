a62cb8eff1f688272b418aace52bc2e36f408957
Yax <1949284+kianby@users.noreply.github.com>
2022-11-27 20:43:06 +0100

    replace unittest by pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import unittest
+import pytest
 
 from stacosys.service import config
 from stacosys.service.configuration import ConfigParameter
@@ -11,31 +11,24 @@ EXPECTED_HTTP_PORT = 8080
 EXPECTED_LANG = "fr"
 
 
-class ConfigTestCase(unittest.TestCase):
-    def setUp(self):        
-        config.put(ConfigParameter.DB_SQLITE_FILE, EXPECTED_DB_SQLITE_FILE)
-        config.put(ConfigParameter.HTTP_PORT, EXPECTED_HTTP_PORT)
+@pytest.fixture
+def init_config():
+    config.put(ConfigParameter.DB_SQLITE_FILE, EXPECTED_DB_SQLITE_FILE)
+    config.put(ConfigParameter.HTTP_PORT, EXPECTED_HTTP_PORT)    
 
-    def test_exists(self):
-        self.assertTrue(config.exists(ConfigParameter.DB_SQLITE_FILE))
+def test_exists(init_config):
+    assert config.exists(ConfigParameter.DB_SQLITE_FILE)
 
-    def test_get(self):
-        self.assertEqual(
-            config.get(ConfigParameter.DB_SQLITE_FILE), EXPECTED_DB_SQLITE_FILE
-        )
-        self.assertEqual(config.get(ConfigParameter.HTTP_HOST), "")
-        self.assertEqual(
-            config.get(ConfigParameter.HTTP_PORT), str(EXPECTED_HTTP_PORT)
-        )
-        self.assertEqual(config.get_int(ConfigParameter.HTTP_PORT), 8080)
-        try:
-            config.get_bool(ConfigParameter.DB_SQLITE_FILE)
-            self.assertTrue(False)
-        except AssertionError:
-            pass
+def test_get(init_config):
+    assert config.get(ConfigParameter.DB_SQLITE_FILE) == EXPECTED_DB_SQLITE_FILE
+    assert config.get(ConfigParameter.HTTP_HOST) == ""
+    assert config.get(ConfigParameter.HTTP_PORT) == str(EXPECTED_HTTP_PORT)
+    assert config.get_int(ConfigParameter.HTTP_PORT) == EXPECTED_HTTP_PORT
+    with pytest.raises(AssertionError):    
+        config.get_bool(ConfigParameter.DB_SQLITE_FILE)
 
-    def test_put(self):
-        self.assertFalse(config.exists(ConfigParameter.LANG))
-        config.put(ConfigParameter.LANG, EXPECTED_LANG)
-        self.assertTrue(config.exists(ConfigParameter.LANG))
-        self.assertEqual(config.get(ConfigParameter.LANG), EXPECTED_LANG)
+def test_put(init_config):
+    assert not config.exists(ConfigParameter.LANG)
+    config.put(ConfigParameter.LANG, EXPECTED_LANG)
+    assert config.exists(ConfigParameter.LANG)
+    assert config.get(ConfigParameter.LANG) == EXPECTED_LANG
