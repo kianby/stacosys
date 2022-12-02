f37a8f797e3ab67949f326dc05829ee0f3caf207
Yax <1949284+kianby@users.noreply.github.com>
2022-12-02 08:59:55 +0100

    improve tests

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -2,10 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 import pytest
+
 from stacosys.service import mailer
 
+
 def test_configure_and_check():
-	mailer.configure_smtp("localhost", 2525, "admin", "admin")
-	mailer.configure_destination("admin@mydomain.com")
-	with pytest.raises(ConnectionRefusedError):
-		mailer.check()
+    mailer.configure_smtp("localhost", 2525, "admin", "admin")
+    mailer.configure_destination("admin@mydomain.com")
+    with pytest.raises(ConnectionRefusedError):
+        mailer.check()
